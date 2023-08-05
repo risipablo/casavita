17fae8a6fe697c375a0a2e40ec44a40997937aaf
Risi <pablomrisi@hotmail.com>
2023-08-05 19:18:23 -0300

    dropdown

--- JS/menu.js
+++ JS/menu.js
@@ -1,1 +1,12 @@
 const toggleMenu = () => document.body.classList.toggle("open");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let quienesSomosLink = document.getElementById("quienesSomos");
+    let submenu = document.getElementById("submenu");
+
+    quienesSomosLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        submenu.classList.toggle("active");
+    });
+});
