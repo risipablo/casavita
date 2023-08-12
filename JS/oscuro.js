04920c99807539cd7a5c8523bdc338a23b01bdfd
Risi <pablomrisi@hotmail.com>
2023-08-11 21:02:00 -0300

    perfiles

--- JS/oscuro.js
+++ JS/oscuro.js
@@ -1,10 +1,34 @@
 
 
 const BtnOscuro = document.querySelectorAll("#oscuro");
+const modoLocalStorageKey = "modoOscuro";
 
-BtnOscuro.forEach(btn => {
-    btn.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
+
+function cambiarModo() {
+    document.body.classList.toggle('dark');
+    BtnOscuro.forEach(btn => {
         btn.classList.toggle('active');
     });
+
+    // variable para guardar en el localstorage
+    const modoActual = document.body.classList.contains('dark') ? 'oscuro' : 'claro';
+    localStorage.setItem(modoLocalStorageKey, modoActual);
+}
+
+// Modo oscuro desde el Local Storage al cargar la página
+const modoGuardado = localStorage.getItem(modoLocalStorageKey);
+if (modoGuardado === 'oscuro') {
+    document.body.classList.add('dark');
+    BtnOscuro.forEach(btn => {
+        btn.classList.add('active');
+    });
+}
+
+BtnOscuro.forEach(btn => {
+    btn.addEventListener('click', cambiarModo);
 });
+
+
+
+
+
