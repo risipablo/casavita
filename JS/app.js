363e904e89cb9ef2466ae0bd8d6fc5f6c6d20af0
Risi <pablomrisi@hotmail.com>
2023-08-17 12:39:35 -0300

    version 1

--- JS/app.js
+++ JS/app.js
@@ -20,3 +20,37 @@
         clickable: false,
         },
     });
+
+
+
+
+
+    // Seccion de contacto 
+    
+    document.getElementById ("btnEnviar").addEventListener("click", function(){
+        let strCorreo = document.getElementById ("idInCe").value;
+        let strNombre = document.getElementById ("idInNom").value;
+        let strMensaje = document.getElementById ("idInMen").value;
+
+        if (strCorreo != "" && strNombre != "" && strMensaje != "") {
+
+            let datos ={
+                c: strCorreo,
+                n: strNombre,
+                m: strMensaje
+            };
+
+            axios.post("/api/contacto", datos)
+            .then(function(response){
+                document.getElementById ("idInCe").value="";
+                document.getElementById ("idInNom").value="";
+                document.getElementById ("idInMen").value="";
+                alert("Su mensaje fue enviado");
+            }).catch(function(error){
+                console.log(error);
+            });
+
+        }else {
+            alert ("Por completar todos los datos");
+        }
+    });
