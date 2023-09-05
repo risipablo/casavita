8ade3b25971ca505de3cb681ea3953ba3e1ba44e
Risi <pablomrisi@hotmail.com>
2023-09-04 23:19:25 -0300

    menu definitivo

--- JS/menu.js
+++ JS/menu.js
@@ -1,3 +1,9 @@
 const toggleMenu = () => document.body.classList.toggle("open");
 
 
+// Cerrar el submenu cuando el cursor se aleja
+const submenuToggle = document.querySelector('.submenu-toggle');
+
+submenuToggle.addEventListener('click', () => {
+    submenuToggle.classList.toggle('active');
+});
