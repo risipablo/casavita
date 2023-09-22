b7ef791bfd820d984c2adf8acb2ce27a2fde6971
Risi <pablomrisi@hotmail.com>
2023-09-22 12:11:04 -0300

    version escritorio

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,25 @@
 
 
-        
-    let swiper = new Swiper(".swiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 2,
-        speed: 800,
-        coverflowEffect: {
+let swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 2,
+    speed: 800,
+    coverflowEffect: {
         rotate: 0,
         stretch: 0,
         depth: 100,
         modifier: 3,
         slideShadows: true,
-        },
-        loop: true,
-        pagination: {
+    },
+    loop: true,
+    pagination: {
         el: ".swiper-pagination",
         clickable: false,
-        },
-    });
-
-
-            
+    },
+    navigation: {
+        nextEl: ".swiper-button-next", // Elemento para la flecha derecha
+        prevEl: ".swiper-button-prev", // Elemento para la flecha izquierda
+    },
+});
